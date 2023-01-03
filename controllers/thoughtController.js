696ad2e05ab2c3f8bb02d3ab609e63fc39147813
edyta0106 <edyta0106@gmail.com>
2023-01-03 16:38:19 -0700

    added getSingleThought to thoughtController.js

---
+++ controllers/thoughtController.js
@@ -0,0 +1,16 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+  // Get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thought) => (!thought ? res.status(404).json({ message: "No thought with that ID" }) : res.json(thought)))
+      .catch((err) => res.status(500).json(err));
+  },
+};
