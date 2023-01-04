44354e06e8e5787ae68401a4a75e67d56b113f95
edyta0106 <edyta0106@gmail.com>
2023-01-04 15:25:11 -0700

    create thought post in thoughtController.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -13,4 +13,18 @@ module.exports = {
       .then((thought) => (!thought ? res.status(404).json({ message: "No thought with that ID" }) : res.json(thought)))
       .catch((err) => res.status(500).json(err));
   },
+
+  // Create a thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => {
+        return User.findOneAndUpdate({ _id: req.body.userId }, { $push: { thoughts: thought._id } });
+      })
+      .then((user) =>
+        !user ? res.status(404).json({ message: "Thought created but found no user with that id" }) : res.json({ message: "Created the thought" })
+      )
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  },
 };
